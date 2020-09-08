a3aab10ba1766a09c9f1823d76217f99f747ea07
Kwasek <kwasek@kwasek.org>
2020-09-08 18:56:39 +0200

    t

--- execute.sh
+++ execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost local:root
-docker run -ti -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v $XDG_RUNTIME_DIR/pulse:/run/pulse:ro --device /dev/dri --entrypoint /bin/bash $1
+docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v $XDG_RUNTIME_DIR/pulse:/run/pulse:ro --device /dev/dri --entrypoint /bin/bash $1
