c1f9bcb92491a5c6db7ca05dced697f128ce55e8
Kwasek <kwasek@kwasek.org>
2020-09-09 21:06:19 +0200

    t

--- code/updater.py
+++ code/updater.py
@@ -7,6 +7,7 @@ import os
 import yaml
 import re
 import datetime
+import difflib
 
 class main():
   mainDir = None
@@ -18,14 +19,19 @@ class main():
     with open('{}/../images.yaml'.format(self.mainDir), 'r') as f:
       self.images = yaml.load(f, Loader=yaml.SafeLoader)
 
-  def getDockerfile(self, dir, dockerfile):
-    logging.info('Reading file: {}/{}'.format(dir, dockerfile))
-    with open('{}/../{}/{}'.format(self.mainDir, dir, dockerfile), 'r') as f:
-      return f.read()
+  def getFile(self, dir, filename):
+    logging.info('Reading file: {}/{}'.format(dir, filename))
+    try:
+      with open('{}/../{}/{}'.format(self.mainDir, dir, filename), 'r') as f:
+        return f.read()
+    except FileNotFoundError:
+      logging.info('Missing file: {}/{}'.format(dir, filename))
+      return ''
   
-  def updateDockerfile(self, dir, dockerfile, content):
-    logging.info('Writing file: {}/{}'.format(dir, dockerfile))
-    with open('{}/../{}/{}'.format(self.mainDir, dir, dockerfile), 'w') as f:
+  def overwriteFile(self, dir, filename, content):
+    logging.info('Writing file: {}/{}'.format(dir, filename))
+    self.checkAndCreateDir(dir)
+    with open('{}/../{}/{}'.format(self.mainDir, dir, filename), 'w') as f:
       return f.write(content)
   
   def getFromVariables(self, content):
@@ -34,20 +40,40 @@ class main():
   def getFromDecode(self, fromValue):
     return fromValue.split()[1].split(':')
   
-  def checkAndUpdateVersionsRegistry(self, content, dir, dockerfile):
-    change = False
-    froms = self.getFromVariables(content)
-    for fromValue in froms:
-      fromImage, fromVersion = self.getFromDecode(fromValue)
-      logging.info('File image: {}, tag: {}'.format(fromImage, fromVersion))
-      latestVersion = self.getRegistryLatest(fromImage)
-      if fromVersion != latestVersion:
-        logging.info('Image: {}, update {} -> {}'.format(fromImage, fromVersion, latestVersion))
-        content = content.replace(fromValue, 'FROM {}:{}'.format(fromImage, latestVersion))
-        change = True
-    if change:
-      self.updateDockerfile(dir, dockerfile, content)
-    return change
+  def checkAndCreateDir(self, dir):
+    if not os.path.exists(dir):
+      logging.info('Dir: {} doesnt exists, creating'.format(dir))
+      os.makedirs(dir)
+  
+  def getMultilineDiff(self, a, b):
+    return ''.join(difflib.ndiff(a.splitlines(1), b.splitlines(1)))
+  
+  def checkAndUpdateVersionsRegistry(self, currentContent, dir, dockerfile, yamlcontent):
+    froms = self.getFromVariables(yamlcontent)
+    fromValue = froms[0]
+    fromImage, fromVersion = self.getFromDecode(fromValue)
+    latestVersion = self.getRegistryLatest(fromImage)
+    yamlcontent = yamlcontent.replace(fromValue, 'FROM {}:{}'.format(fromImage, latestVersion))
+    if yamlcontent != currentContent:
+      logging.info('Updating image')
+      logging.info("DIFF:\n{}".format(self.getMultilineDiff(currentContent, yamlcontent)))
+      self.overwriteFile(dir, dockerfile, yamlcontent)
+      self.checkAndUpdateVersionFile(dir, latestVersion)
+      return True
+    return False
+  
+  def checkAndUpdateVersionFile(self, dir, version):
+    versionFile = 'version'
+    content = self.getFile(dir, versionFile)
+    if content == '' or version != ''.join(content.split('.')[:-1]):
+      versionWithBuild = '{}.01'.format(version)
+    else:
+      versionWithBuild = '{}.{:02d}'.format(version, int(content.split('.')[-1])+1)
+    if content != versionWithBuild:
+      logging.info('Updating version')
+      logging.info("DIFF:\n{}".format(self.getMultilineDiff(content, versionWithBuild)))
+      self.overwriteFile(dir, versionFile, versionWithBuild)
+
 
   def getRegistryLatest(self, fromImage):
     if fromImage.find('/') == -1:
@@ -77,8 +103,8 @@ class main():
   def main(self):
     logging.info(self.images)
     for template in self.images['templates']:
-      content = self.getDockerfile(template['dir'], template['dockerfile'])
-      updateStatus = self.checkAndUpdateVersionsRegistry(content, template['dir'], template['dockerfile'])
+      content = self.getFile(template['dir'], 'Dockerfile')
+      updateStatus = self.checkAndUpdateVersionsRegistry(content, template['dir'], 'Dockerfile', template['dockerfilecontent'])
       for image in template['images']:
         print(image)
 
